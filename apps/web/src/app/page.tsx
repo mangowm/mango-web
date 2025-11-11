52af882627bf299839ef7daa7f8974c73eb8a94b
atheeq-rhxn <atheeq.rhxn@gmail.com>
2025-11-11 18:04:40 +0530

    🔧 chore: remove tony tutorial from homepage

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,14 +2,12 @@ import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
 import { Highlights } from "@/components/highlights";
-import { VideoTutorial } from "@/components/video-tutorial";
 
 export default function Home() {
 	return (
 		<main id="main" className="min-h-screen bg-background">
 			<Header />
 			<Hero />
-			<VideoTutorial />
 			<Highlights />
 			<Footer />
 		</main>
