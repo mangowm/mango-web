acf0223f096d6f7b988e0d7f3e5953d4b32b4858
atheeq-rhxn <atheeq.rhxn@gmail.com>
2025-12-05 21:51:45 +0530

    feat: docs written and fumadocs setup

--- apps/web/.source/server.ts
+++ apps/web/.source/server.ts
@@ -1,22 +1,28 @@
 // @ts-nocheck
-import * as __fd_glob_18 from "../content/docs/getting-started/installation.mdx?collection=docs"
-import * as __fd_glob_17 from "../content/docs/getting-started/index.mdx?collection=docs"
-import * as __fd_glob_16 from "../content/docs/getting-started/basic-setup.mdx?collection=docs"
-import * as __fd_glob_15 from "../content/docs/features/layouts.mdx?collection=docs"
-import * as __fd_glob_14 from "../content/docs/features/ipc.mdx?collection=docs"
-import * as __fd_glob_13 from "../content/docs/features/integrations.mdx?collection=docs"
-import * as __fd_glob_12 from "../content/docs/features/index.mdx?collection=docs"
-import * as __fd_glob_11 from "../content/docs/features/animations.mdx?collection=docs"
-import * as __fd_glob_10 from "../content/docs/configuration/window-management.mdx?collection=docs"
-import * as __fd_glob_9 from "../content/docs/configuration/input-devices.mdx?collection=docs"
-import * as __fd_glob_8 from "../content/docs/configuration/index.mdx?collection=docs"
-import * as __fd_glob_7 from "../content/docs/configuration/core-settings.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/configuration/appearance.mdx?collection=docs"
-import * as __fd_glob_5 from "../content/docs/configuration/advanced.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/index.mdx?collection=docs"
-import { default as __fd_glob_3 } from "../content/docs/getting-started/meta.json?collection=docs"
-import { default as __fd_glob_2 } from "../content/docs/features/meta.json?collection=docs"
-import { default as __fd_glob_1 } from "../content/docs/configuration/meta.json?collection=docs"
+import * as __fd_glob_24 from "../content/docs/window-management/scratchpad.mdx?collection=docs"
+import * as __fd_glob_23 from "../content/docs/window-management/rules.mdx?collection=docs"
+import * as __fd_glob_22 from "../content/docs/window-management/overview.mdx?collection=docs"
+import * as __fd_glob_21 from "../content/docs/window-management/layouts.mdx?collection=docs"
+import * as __fd_glob_20 from "../content/docs/configuration/monitors.mdx?collection=docs"
+import * as __fd_glob_19 from "../content/docs/configuration/input.mdx?collection=docs"
+import * as __fd_glob_18 from "../content/docs/configuration/basics.mdx?collection=docs"
+import * as __fd_glob_17 from "../content/docs/appearance/theming.mdx?collection=docs"
+import * as __fd_glob_16 from "../content/docs/appearance/effects.mdx?collection=docs"
+import * as __fd_glob_15 from "../content/docs/appearance/animations.mdx?collection=docs"
+import * as __fd_glob_14 from "../content/docs/bindings/mouse-gestures.mdx?collection=docs"
+import * as __fd_glob_13 from "../content/docs/bindings/keys.mdx?collection=docs"
+import * as __fd_glob_12 from "../content/docs/advanced/waybar.mdx?collection=docs"
+import * as __fd_glob_11 from "../content/docs/advanced/ipc.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/advanced/integrations.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/quick-start.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/installation.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/faq.mdx?collection=docs"
+import { default as __fd_glob_5 } from "../content/docs/window-management/meta.json?collection=docs"
+import { default as __fd_glob_4 } from "../content/docs/configuration/meta.json?collection=docs"
+import { default as __fd_glob_3 } from "../content/docs/appearance/meta.json?collection=docs"
+import { default as __fd_glob_2 } from "../content/docs/bindings/meta.json?collection=docs"
+import { default as __fd_glob_1 } from "../content/docs/advanced/meta.json?collection=docs"
 import { default as __fd_glob_0 } from "../content/docs/meta.json?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../source.config';
@@ -26,4 +32,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "configuration/meta.json": __fd_glob_1, "features/meta.json": __fd_glob_2, "getting-started/meta.json": __fd_glob_3, }, {"index.mdx": __fd_glob_4, "configuration/advanced.mdx": __fd_glob_5, "configuration/appearance.mdx": __fd_glob_6, "configuration/core-settings.mdx": __fd_glob_7, "configuration/index.mdx": __fd_glob_8, "configuration/input-devices.mdx": __fd_glob_9, "configuration/window-management.mdx": __fd_glob_10, "features/animations.mdx": __fd_glob_11, "features/index.mdx": __fd_glob_12, "features/integrations.mdx": __fd_glob_13, "features/ipc.mdx": __fd_glob_14, "features/layouts.mdx": __fd_glob_15, "getting-started/basic-setup.mdx": __fd_glob_16, "getting-started/index.mdx": __fd_glob_17, "getting-started/installation.mdx": __fd_glob_18, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "advanced/meta.json": __fd_glob_1, "bindings/meta.json": __fd_glob_2, "appearance/meta.json": __fd_glob_3, "configuration/meta.json": __fd_glob_4, "window-management/meta.json": __fd_glob_5, }, {"faq.mdx": __fd_glob_6, "index.mdx": __fd_glob_7, "installation.mdx": __fd_glob_8, "quick-start.mdx": __fd_glob_9, "advanced/integrations.mdx": __fd_glob_10, "advanced/ipc.mdx": __fd_glob_11, "advanced/waybar.mdx": __fd_glob_12, "bindings/keys.mdx": __fd_glob_13, "bindings/mouse-gestures.mdx": __fd_glob_14, "appearance/animations.mdx": __fd_glob_15, "appearance/effects.mdx": __fd_glob_16, "appearance/theming.mdx": __fd_glob_17, "configuration/basics.mdx": __fd_glob_18, "configuration/input.mdx": __fd_glob_19, "configuration/monitors.mdx": __fd_glob_20, "window-management/layouts.mdx": __fd_glob_21, "window-management/overview.mdx": __fd_glob_22, "window-management/rules.mdx": __fd_glob_23, "window-management/scratchpad.mdx": __fd_glob_24, });
